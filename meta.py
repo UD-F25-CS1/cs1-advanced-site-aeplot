80a9fe5e7df9f4517ae2539238c49eea93caa409
Addison Plotkowski <aeplot@udel.edu>
2025-12-06 15:35:25 -0500

    hide debug

--- meta.py
+++ meta.py
@@ -5,7 +5,7 @@ You can set site-wide information and settings here.
 
 from drafter import *
 
-# hide_debug_information()
+hide_debug_information()
 set_website_title('energysim.exe')
 set_website_framed(False)
 set_site_information(
