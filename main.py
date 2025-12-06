9eb210b8de3e0cc3b485a836dc2a4da5b44c2141
Addison Plotkowski <aeplot@udel.edu>
2025-12-06 15:51:14 -0500

    attempted to fix index problem

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ messages = [
 
 @route
 def index(state: State) -> Page:
+    state.count = 0
     return Page(state, [
         Header('⚡️ Energy Simulator ⚡️',4),
         Button('''Start''',url = '/main_game'),
